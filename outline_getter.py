ca9f00f6d4a4a74d6e49dd954add7ad8b4fde302
cwang <cwang@cwang-XPS13-reborn.(none)>
2014-10-11 17:18:08 -0400

    main scripts

---
+++ outline_getter.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+# PROCEDURE: Read from list of URLS -> grab first two paragraphs
+import time
+import datetime
+import re
+from bs4 import BeautifulSoup
+import itertools
+from multiprocessing import Pool
+import requests
+from os import path, pardir, makedirs, chdir, remove, getcwd
+import csv
+
+start_time = datetime.datetime.now().time().isoformat()
+
+source_path = path.join(getcwd(),"wikipedia")
+chdir(source_path)
+
+file_name = "List_of_academic_disciplines_and_sub_disciplines.html"
+
+souper = open(file_name,'r').read()
+souper = BeautifulSoup(souper)
+
+outline_list = []
+master_list = []
+deduplicate_list = []
+
+all_urls = souper.findAll('a')
+
+for url_frag in all_urls:
+    url = url_frag.attrs.get('href')
+    #print url
+    name = str(url)
+    if name[:6] == "/wiki/" and "#" not in name and ":" not in name:
+        name = url.split('/wiki/')[-1]
+        text_name = url_frag.getText()
+        name = name.replace("_"," ")
+        url = "http://en.wikipedia.org" + url
+        try:
+            if "Outline" in name and name not in deduplicate_list:
+                item_dict = dict(
+                    name=name,
+                    url=url,
+                )
+                deduplicate_list.append(name)
+                outline_list.append(item_dict)
+            if str(name) == str(text_name) and "Outline" not in name and name not in deduplicate_list:
+                print name,"|",text_name,"|",url
+                item_dict = dict(
+                    name=name,
+                    url=url,
+                )
+                master_list.append(item_dict)
+                deduplicate_list.append(name)
+        except:
+            pass
+
+c = csv.writer(open("outline_list_fields.csv", "wb"))
+c.writerow(["field","link",])
+
+print len(outline_list)
+#raw_input()
+
+for item in outline_list:
+    c.writerow([item['name'],item['url']])
+    url = item['url']
+    name = item['name']
+    page = requests.get(url,timeout=30)
+    souper = BeautifulSoup(page.content)
+    souper = str(souper)
+    filename = str(name.replace(' ','_')) + ".html"
+    open(filename,'w').write(souper)
+    print filename,"FILE SAVED"
+
+print len(master_list)
+
+c = csv.writer(open("main_list_fields.csv", "wb"))
+c.writerow(["field","link",])
+
+for item in master_list:
+    c.writerow([item['name'],item['url']])
+
+
+
+print ""
+print ""
+print "Start time: " + str(start_time)
+print "End time: " + str(datetime.datetime.now().time().isoformat())
+print ""
+print ""
---
+++ subject_outline_parser.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# PROCEDURE: Read from list of URLS -> grab first two paragraphs
+import time
+import datetime
+import re
+from bs4 import BeautifulSoup
+import itertools
+from multiprocessing import Pool
+import requests
+from os import path, pardir, makedirs, chdir, remove, getcwd, listdir
+import csv
+
+start_time = datetime.datetime.now().time().isoformat()
+
+source_path = path.join(getcwd(),"wikipedia")
+chdir(source_path)
+
+source_contents = listdir(source_path)
+
+print source_contents
+
+master_list = []
+deduplicate_list = []
+
+for item in source_contents:
+    if item.endswith('.html'):
+        souper = open(item,'r').read()
+        souper = BeautifulSoup(souper)
+        #print souper
+        all_urls = souper.findAll('a')
+
+        for url_frag in all_urls:
+            url = url_frag.attrs.get('href')
+            #print url
+            try:
+                name = str(url)
+                if name[:6] == "/wiki/" and "#" not in name and ":" not in name:
+                    name = url.split('/wiki/')[-1]
+                    text_name = url_frag.getText()
+                    name = name.replace("_"," ")
+                    url = "http://en.wikipedia.org" + url
+                    try:
+                        if str(name) == str(text_name) and "Outline" not in name and name not in deduplicate_list:
+                            print name,"|",text_name,"|",url
+                            item_dict = dict(
+                                name=name,
+                                url=url,
+                            )
+                            master_list.append(item_dict)
+                            deduplicate_list.append(name)
+                    except:
+                        pass
+            except:
+                pass
+
+print len(master_list)
+
+c = csv.writer(open("ancillary_list_fields.csv", "wb"))
+c.writerow(["field","link",])
+
+for item in master_list:
+    c.writerow([item['name'],item['url']])
+
+
+print ""
+print ""
+print "Start time: " + str(start_time)
+print "End time: " + str(datetime.datetime.now().time().isoformat())
+print ""
+print ""
