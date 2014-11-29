6ecb1041e5f5a78b3eea0de744584fb15808ff8d
cwang <cwang@cwang-XPS13-reborn.(none)>
2014-11-29 18:06:50 -0500

    derp

--- interrelation_parser.py
+++ interrelation_parser.py
@@ -2,22 +2,18 @@
 import datetime
 from bs4 import BeautifulSoup
 import requests
-from os import path, chdir, getcwd
+from os import path, chdir, getcwd, listdir
 import csv
+import string
 
 start_time = datetime.datetime.now().time().isoformat()
 
-source_path = path.join(getcwd(),"wikipedia")
-chdir(source_path)
+chdir(path.join(getcwd(),"wikipedia"))
 
 csvs_dict = dict(
     ancillary = "ancillary_list_fields.csv",
     main = "main_list_fields.csv",
 )
-
-output_path = path.join(getcwd(),"linkage_pages")
-
-#get main subject linkages:
 
 main_csv_list = csvs_dict['main']
 
@@ -26,6 +22,25 @@ master_subject_list = []
 with open(main_csv_list,"rU") as f:
     vals = csv.DictReader(f)
     for i in vals:
-        print i['field']
-        master_subject_list.append(i['field'])
-        linkage_page = requests.get()
+        master_subject_list.append(str(i['field']).lower())
+
+chdir(path.join(getcwd(),"linkage_pages"))
+
+c = csv.writer(open("interrelations.csv",'wb'))
+c.writerow(['field','interrelation'])
+
+for file in listdir(getcwd()):
+    if file.endswith('.html'):
+        file_name = str(file).replace('.html',"")
+        file_blob = ""
+        for element in file_name.split('_')[:-1]:
+            file_blob = file_blob + element + " "
+        file_blob = str(file_blob[:-1].lower())
+        if file_blob in master_subject_list:
+            souper = open(file,'r').read()
+            souper = BeautifulSoup(souper)
+            for link in souper.findAll('ul')[0].findAll('li'):
+                linktext = str(filter(lambda x: x in string.printable, link.getText().replace('(links)',"").strip().lower())).strip()
+                if linktext in master_subject_list:
+                    print file_blob + "|"+ linktext
+                    c.writerow([file_blob,linktext])
